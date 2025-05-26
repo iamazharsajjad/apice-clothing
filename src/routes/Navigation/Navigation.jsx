cbbe7644aa37b5c92135c09f05c32ef40ea76a3f
Azhar Sajjad <iamazharsajjad@gmail.com>
2025-05-27 03:01:26 +0500

    [108] Signout integrated

--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -2,9 +2,15 @@ import { Fragment,useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as Crown } from '../../assets/crown.svg';
 import { UserContext } from '../../context/user.context';
+import { signOutUser } from '../../utils/Firebase/FirebaseUtils'
 import './style.scss'
 const Navigation = ()=>{
-    const { currentUser } = useContext( UserContext );
+    const { currentUser, setCurrentUser } = useContext( UserContext );
+    const signOutHandler =  async() => {
+        const res = await signOutUser();
+        setCurrentUser(null);
+        console.log(res);
+    }
     return(
       <Fragment>
         <div className='navigation'>
@@ -15,9 +21,13 @@ const Navigation = ()=>{
                 <Link className='nav-link' to='/shop'>
                     SHOP
                 </Link>
-                <Link className='nav-link' to='/auth'>
-                    SIGN IN
-                </Link>
+                {
+                    currentUser ? (
+                        <span className='nav-link' onClick = {signOutHandler}>SIGN OUT</span>
+                    ) : ( <Link className='nav-link' to='/auth'>
+                        SIGN IN
+                    </Link>)
+                }
             </div>        
         </div>
         <Outlet />
